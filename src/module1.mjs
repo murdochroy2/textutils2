493c1a0b66daa7c9b7518835fcb3abb53aa540cb
RohiModak <rohi@localhost.localdomain>
2023-07-19 20:43:56 +0530

    1. Modules to test import type added 2. Navbar added

---
+++ src/module1.mjs
@@ -0,0 +1,5 @@
+import abc, {a, b, c} from "./module2.mjs"
+
+console.log(abc);
+console.log(b);
+console.log(c);
---
+++ src/module2.mjs
@@ -0,0 +1,6 @@
+const a = "Rohi"
+const b = "Modak"
+const c = "C++"
+const d = "DB"
+export default b
+export {a, b, c}
