e4053821b51a0212208cae72468ad5d529207080
Rohi Modak <rohi@localhost>
2023-07-23 13:53:45 +0530

    Navbar dark mode

--- src/App.js
+++ src/App.js
@@ -3,14 +3,16 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import About from "./components/About";
+import { useState } from "react";
 
 function App() {
   let name = "Rohi";
+  const [mode, setMode] = useState("light")
   return (
     <>
-      <Navbar title={"The Title"} aboutText="About"/>
+      <Navbar title={"The Title"} aboutText="About" mode={mode}/>
       <div className="container my-2">
-        <TextForm heading="Enter text to analyze here"/>
+        <TextForm heading="Enter text to analyze here" mode={mode} />
         {/* <About /> */}
       </div>
     </>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 
 function Navbar(props) {
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary"  data-bs-theme="dark">
+    <nav className="navbar navbar-expand-lg bg-body-tertiary"  data-bs-theme={`${props.mode}`}>
       <div className="container-fluid">
         <a className="navbar-brand" href="/">
           {props.title}
