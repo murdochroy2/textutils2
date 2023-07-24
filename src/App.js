a7d01185c09d2e29be5441bb8c24abbf92f9eb04
Rohi Modak <rohi@localhost>
2023-07-24 21:29:18 +0530

    Alerts started

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import About from "./components/About";
 import { useState } from "react";
+import Alert from "./components/Alert";
 
 function App() {
   let name = "Rohi";
@@ -12,14 +13,21 @@ function App() {
     if (mode === "light") {
       setMode("dark")
       document.body.style.backgroundColor = "#042743"
+      setAlert("success", "Dark mode enabled")
     } else {
       setMode("light")
       document.body.style.backgroundColor = "white"
+      setAlert("success", "Dark mode disabled")
     }
+  }
+  const [alert, setAlert] = useState(null)
+  const showAlert = (message, type) => {
+
   }
   return (
     <>
       <Navbar title={"The Title"} aboutText="About" mode={mode} toggleMode={toggleMode}/>
+      <Alert showAlert={showAlert} alert={alert}/>
       <div className="container my-2">
         <TextForm heading="Enter text to analyze here" mode={mode} />
         {/* <About /> */}
